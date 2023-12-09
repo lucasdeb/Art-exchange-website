4f1f042296e0bdadd65f911d724843b11ad35f30
Lucas Debarbieri <lucas1702debarbieri@gmail.com>
2023-12-09 17:18:18 -0300

    Funcionalidad editar el precio del arte y varios

--- assets/common/getdestacadas.php
+++ assets/common/getdestacadas.php
@@ -1,28 +1,31 @@
 <?php
 include('config.php');
 
+//Un arreglo de todas las imagenes o arte de la base de datos y devuelve los mas recientes
+
 $sql = "SELECT Id_Arte, User_Id, img_arte, precio FROM Arte";
 
 $result = $link->query($sql);
 
-function destacadas($result){
+function destacadas($result)
+{
     $dest = array();
     while ($row = mysqli_fetch_assoc($result)) {
         $cont = '<div class="colecciones-destacada-1">
                 <img src="../imgs/icons/fire.svg" alt="" class="iconos" style="width: 20px;"><label for="">Destacada</label>
                 <div class="medio">
                     <div class="izquierda">
-                        <h1>ID: '.$row["Id_Arte"].'</h1>
+                        <h1>ID: ' . $row["Id_Arte"] . '</h1>
                     </div>
-                    <img src="'.$row["img_arte"].'" alt="">
-                    <h3>Precio: $'.$row["precio"].'</h3>
-                    <a href="../common/payment.php?id_exac='.$row["Id_Arte"].'">Comprar</a>
+                    <img src="' . $row["img_arte"] . '" alt="">
+                    <h3>Precio: $' . $row["precio"] . '</h3>
+                    <a href="../common/payment.php?id_exac=' . $row["Id_Arte"] . '">Comprar</a>
                 </div>
             </div>';
-        array_push($dest,$cont);
+        array_push($dest, $cont);
     }
-    rsort($dest);
-    return $dest;
+    rsort($dest, 1);
+    return array_reverse($dest);
 }
 
 mysqli_close($link);
--- assets/common/miInventario.php
+++ assets/common/miInventario.php
@@ -1,7 +1,5 @@
 <?php
 
-//terminar de implementar funcionalidad editar
-
 include('config.php');
 
 $sql = "SELECT Id_Arte, User_Id, Privado, img_arte, Precio FROM Arte";
@@ -22,20 +20,20 @@ function inventario($result)
                     <img src="' . $row['img_arte'] . '">
                     <h3>Privado: ' . $row['Privado'] . '</h3>
                     <h3>Precio: ' . $row['Precio'] . '</h3>
-                    <button class="ver-perfil" onclick="abrir()" value="' . $row['Id_Arte'] . '">Editar</button>
+                    <button class="ver-perfil" onclick="abrirEdicion(' . $row['Id_Arte'] . ')">Editar</button>
                 </div>';
         }
     }
 
     echo '</div>
     <div class="editar-arte" id="editar-arte">
-        <form action="precio.php" method="post" class="editar-arte-formulario">
-        <h1>Editar ID: ' . $row['Id_Arte'] . '</h1>
-            <label for="precio"><b>Precio: </b></label>
-            <input type="number" placeholder="Editar Precio" name="precio" min="0" step="1">
+        <form action="precio.php" method="POST" class="editar-arte-formulario">
+        <h1>Editar</h1>
+            <label for="precio" ><b>Precio: </b></label>
+            <input type="number" placeholder="Editar Precio" name="precio" min="0" step="1" required>
             <input type="checkbox" id="privado" name="Privado" value="1">
             <label for="privado">Privado: </label>
-            <input type="hidden" name="id_arte" value="' . $row['Id_Arte'] . '">
+            <input type="hidden" id="id_arte_input" name="id_arte" value="">
             <button type="submit">Aceptar</button>
             <button type="button" onclick="cerrar()">Cancelar</button>
         </form>
